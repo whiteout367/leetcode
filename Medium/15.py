2336ad075703abcd7cb538d5b6b67ea4691cddd6
white467 <jwj78772@gmail.com>
2023-08-01 23:47:48 +0900

    fail 15

---
+++ Medium/15.py
@@ -0,0 +1,11 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        """
+        two point
+        1. 리스트에서 값 하나씩 가져오기
+        2. 다음부터 투포인터 사용
+        3. 끝에서 마지막까지
+
+        fail
+        """
+        
